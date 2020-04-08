111482336f0b127a7429b96e91d15c9eb0aba0d2
luciodarcon <luciiodarcon@gmail.com>
2020-04-07 22:01:35 -0300

    edit menu

--- app/src/main/java/com/example/controlsales/fragments/PanelFragment.kt
+++ app/src/main/java/com/example/controlsales/fragments/PanelFragment.kt
@@ -4,18 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.controlsales.R
 
-class PanelFragment : Fragment() {
+class PanelFragment : Fragment(){
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.panel_fragment, container, false)
-    }
+    ): View {
 
+        val view = inflater.inflate(R.layout.panel_fragment, container, false)
+        val btn = view.findViewById<Button>(R.id.btnFragmentPanel)
+
+
+        btn.setOnClickListener(View.OnClickListener {
+            Toast.makeText(context, "Ola mundo Fragment!", Toast.LENGTH_LONG).show()
+        })
+
+        return view
+    }
 
 }
--- app/src/main/java/com/example/controlsales/view/PanelActivity.kt
+++ app/src/main/java/com/example/controlsales/view/PanelActivity.kt
@@ -2,6 +2,7 @@ package com.example.controlsales.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
